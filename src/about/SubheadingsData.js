6ee1f47d6a716005cf7715c347f9b27391e13c3a
jinlee98 <jkl9515@gmail.com>
2023-09-07 16:02:45 -0700

    styling changes

--- src/about/SubheadingsData.js
+++ src/about/SubheadingsData.js
@@ -41,17 +41,19 @@ const subheadingsData = {
                         May 2022 ~ Aug 2022
                     </p>
                     <p>
-                        • Designed, implemented, tested, and deployed a full-stack project which provides visibility
-                        into planning entities and saves Amazon hundreds - thousands of dollars each month in working-hours. 
+                    • Designed, implemented, tested, and deployed a full-stack project which provides visibility into planning entities and
+saves Amazon thousands of dollars each month in working-hours. Fully documented
                     </p>
                     <p>
-                        • Intuitive front end web app displaying, filtering, and querying planning entities which were previously manually found
-                        from logs - S3, Cloudfront to use a serverless architecture, caching data on user edge servers for very fast load and cache times. 
-                        Typescript, React, Jest, and Enzyme 
+                    • Intuitive single-page web app using TypeScript, React, Amazon Katal, S3, Cloudfront to use a serverless
+architecture, caching data on user edge servers, resulting in 23% faster load and cache times and better scalability
+than initial proposed server design. Tested with React testing library, Jest, and Enzyme 
                     </p>
                     <p>
-                        • Back end infrastructure also using serverless architecture to implement file ingestion, event ingestion, and querying API for the front end.
-                        Java, Honeycomb, DynamoDB
+                    • Back end infrastructure also using serverless architecture using DynamoDB, Amazon Honeycomb and Java to
+implement file ingestion, event ingestion, and querying with RESTful API integration for the front end with
+average 58ms API response time on queries.
+
                     </p>
                 </>
             )
@@ -64,16 +66,19 @@ const subheadingsData = {
                         May 2021 ~ Aug 2021
                     </p>
                     <p>
-                        • Inherited a barebones project, transformed it into a polished product, and published a beta version onto the App
-                        Store
+                    • As the sole developer on the team at the start of the product’s development - transformed Figma mockups and the
+managers’ vision into a polished, modern app - keeping in mind scalability and providing my opinions as a
+developer to the team, and published a beta version onto the App Store, with full set of documentation
                     </p>
                     <p>
-                        • Designed custom components with React Hooks, designed features, and brought requested features into the app using both existing libraries and custom components
-                        resulting in clean and efficient UI. React Native, JavaScript.
+                    • Front end development in React Native and JavaScript, designing custom components with React Hooks, with
+focus on custom components to minimize excess packages reducing app size by 12% and increasing reusability.
                     </p>
                     <p>
-                        • Connected user progress and lessons to database, connected various platforms, integrated social media for better user experience, and designed RESTful API’s for seamless
-                        and intuitive lesson plans for users. JavaScript, PostgreSQL.
+                    • Back end development in JavaScript and PostgreSQL, designing efficient relational databases
+(19% reduction in database size with new design), connecting user progress and lesson content to databases,
+integrating social media for better user experience, and designing RESTful API’s for seamless and intuitive lesson
+plans for users
                     </p>
                 </>
             )
@@ -86,20 +91,21 @@ const subheadingsData = {
                         Sept 2020 ~ Apr 2021
                     </p>
                     <p>
-                        • Created & followed structured test plans and test cases to validate operation for new features & changes,
-                        documented test coverage, bugs, and risks to development team
+                    • Created & followed structured test plans and test cases to validate operation for new features & changes,
+documented test coverage, bugs, and risks to development team, finding an average of 4.3 bugs per version release
                     </p>
                     <p>
-                        • Developed and tested features for web and mobile to deliver an engaging and effective experience for customers and merchants of the app. 
-                        React Native, React, JavaScript
+                    • Front end development in React Native, React, and JavaScript. Increased merchant satisfaction by 33% with my
+new design for merchant onboarding
                     </p>
                     <p>
-                        • Used RESTful practices to design good API’s that connect
-                        the clients and the server. JavaScript, Spring, PHP.
+                    • Back end development in JavaScript, Spring and PHP designing and implementing RESTful API’s
+reducing data transfer times by 12%
                     </p>
                     <p>
-                        • Worked with AWS and Azure to store and backup user data, update app
-                        content, and test various features in staging environment
+                    • Work with AWS and Azure: mainly DynamoDB and CosmosDB to store and backup user data, update app content
+for customers, and test various features in staging environment
+
                     </p>
                 </>
             )
